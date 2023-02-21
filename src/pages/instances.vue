1941b4b9334d9f674cf4a5d78c8cc5e05c215273
Jonas Schneider <schneider-jonas@mein.gmx>
2023-02-22 00:08:44 +0100

    save

--- src/pages/instances.vue
+++ src/pages/instances.vue
@@ -15,7 +15,7 @@
                         <p class="text-xl m-0 hidden md:block">8000</p>
                         <p class="m-0">RAM</p>
                     </div>
-                    <ProgressBar class="w-11 h-1rem border-round-md" value="50"></ProgressBar>
+                    <ProgressBar class="w-11 h-1rem border-round-md" value="50" :showValue="false"></ProgressBar>
                 </div>
                 <div
                     class="w-6 p-4 surface-card h-full border-round-md flex flex-column justify-content-center align-items-center">
@@ -25,14 +25,18 @@
                         <p class="text-xl m-0 hidden md:block">100</p>
                         <p class="m-0">CPU</p>
                     </div>
-                    <ProgressBar class="w-11 h-1rem border-round-md w-12" value="15"></ProgressBar>
+                    <ProgressBar class="w-11 h-1rem border-round-md w-12" value="15" :showValue="false"></ProgressBar>
                 </div>
             </div>
             <div
                 class="flex-auto relativ justify-content-between mb-3 mx-4 gap-3 flex flex-column md:flex-row fadein animation-duration-200">
-                <div class="w-full md:w-4 surface-card min-h-0 md:h-full border-round-md relativ p-2">
+                <div class="w-full md:w-4 surface-card md:h-full border-round-md relativ p-2">
                     <template v-for="i in 5" :key="i">
-                        <div class="w-full h-5rem surface-100 border-round-sm mb-2"></div>
+                        <div class="w-full h-5rem border-round-sm mb-2 p-3 bg-instance-item cursor-pointer flex align-items-center gap-3 select-none">
+                            <Avatar icon="pi pi-server green-600" :style="{backgroundColor: Math.random() > 0.5 ? 'var(--primary-color)' : ''}"/>
+                            <p class="flex-auto text-sm">Studienplan</p>
+                            <Button icon="pi pi-angle-right" class="p-button-xs p-button-rounded surface-200 text-white"></Button>
+                        </div>
                     </template>
                 </div>
                 <div class="w-full md:w-8 surface-card h-full border-round-md">
@@ -75,3 +79,19 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.bg-instance-item {
+    background-color: rgba(255, 255, 255, 0.05);
+    transition: all 100ms;
+
+    &:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+        transform: scale(1.025);
+    }
+}
+
+.p-button-xs {
+    transform: scale(0.75) !important;
+}
+</style>
