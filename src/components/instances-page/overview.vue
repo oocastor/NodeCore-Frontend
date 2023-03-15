1ab29f1051e54b1202e121e46d548992561dc793
Jonas Schneider <schneider-jonas@mein.gmx>
2023-03-15 22:14:14 +0100

    save

---
+++ src/components/instances-page/overview.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="px-1 py-2">
+        <p class="m-0 mb-3 font-mono" style="font-size: 1rem;">Overview</p>
+        <div class="flex gap-2">
+            <div class="flex flex-column flex-auto">
+                <p class="m-0 font-mono text-xs mb-2">Instances</p>
+                <div class="flex gap-2">
+                    <countItem str="Running" num="2" color="var(--green-400)"></countItem>
+                    <countItem str="Stopped" num="8" color="var(--red-400)"></countItem>
+                </div>
+            </div>
+            <div class="flex flex-column flex-auto">
+                <p class="m-0 font-mono text-xs mb-2">Redirects</p>
+                <div class="flex gap-2">
+                    <countItem str="Enabled" num="4" color="var(--surface-400)"></countItem>
+                    <countItem str="Disabled" num="0" color="var(--surface-400)"></countItem>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import countItem from "@/components/instances-page/countItem.vue";
+
+export default {
+    components: {
+        countItem
+    }
+}
+</script>
--- src/pages/instances.vue
+++ src/pages/instances.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-column h-full">
+    <div class="flex flex-column h-full" style="min-width: 400px;">
         <div class="w-full surface-card flex justify-content-between align-items-center px-3" style="min-height: 60px;">
             <div class="text-4xl font-grandHotel">nodeup</div>
-            <Avatar class="bg-primary" label="Ca" @click="toggleAvatarMenu"></Avatar>
+            <Avatar class="surface-section cursor-pointer" icon="pi pi-user" @click="toggleAvatarMenu"></Avatar>
         </div>
         <Menu ref="menu" :model="items" :popup="true"></Menu>
         <div class="flex-auto flex w-full flex-column mx-auto" style="max-width: 900px;">
@@ -13,12 +13,7 @@
             <div
                 class="flex-auto relativ md:justify-content-between mb-3 mx-4 gap-3 flex flex-column md:flex-row fadein animation-duration-200">
                 <div class="w-full md:w-8 flex flex-column surface-card h-min border-round-md md:hidden gap-2 p-2">
-                    <p class="my-2 ml-1 font-mono" style="font-size: 1rem;">Overview</p>
-                    <div class="flex gap-2">
-                        <countItem str="Running" num="2" color="var(--green-400)"></countItem>
-                        <countItem str="Stopped" num="8" color="var(--red-400)"></countItem>
-                        <countItem str="Total" num="10" color="var(--surface-400)"></countItem>
-                    </div>
+                    <overview></overview>
                 </div>
                 <div class="w-full md:w-4 md:h-full flex flex-column gap-3">
                     <div class="surface-card p-2 border-round-md relativ">
@@ -35,7 +30,7 @@
                         <div class="w-full mt-2 mb-3 flex justify-content-between align-items-center">
                             <p class="m-0 ml-1 font-mono" style="font-size: 1rem;">Redirects</p>
                             <Button icon="pi pi-plus" class="-m-1 -mt-2 bg-white-a15 text-color"
-                                style="transform: scale(0.7);"></Button>
+                                style="transform: scale(0.7);" @click="createRedirect();"></Button>
                         </div>
                         <objListItem v-for="i in 2" :key="i" icon="pi-arrow-right-arrow-left text-xs" name="Code-Server"
                             :active="Math.random() > 0.5">
@@ -43,15 +38,10 @@
                     </div>
                 </div>
                 <div class="w-full md:w-8 hidden flex-column surface-card h-min border-round-md md:flex gap-2 p-2">
-                    <!-- <p class="my-2 ml-1 font-mono" style="font-size: 1rem;">Overview</p>
-                    <div class="flex gap-2">
-                        <countItem str="Running" :num="2" color="var(--green-400)"></countItem>
-                        <countItem str="Stopped" :num="8" color="var(--red-400)"></countItem>
-                        <countItem str="Total" :num="10" color="var(--surface-400)"></countItem>
-                    </div> -->
-                    <redirectUpdateView :isCreation="true"></redirectUpdateView>
-                    <!-- <instanceUpdateView :isCreation="false"></instanceUpdateView> -->
-                    <!-- <instanceView></instanceView> -->
+                    <overview v-if="view == 0"></overview>
+                    <instanceView v-if="view == 1"></instanceView>
+                    <redirectUpdateView :isCreation="true" v-if="view == 2"></redirectUpdateView>
+                    <instanceUpdateView :isCreation="false" v-if="view == 3"></instanceUpdateView>
                 </div>
             </div>
             <div class="w-full h-3rem flex justify-content-center align-items-center gap-1">
@@ -73,6 +63,7 @@ import countItem from "@/components/instances-page/countItem.vue";
 import instanceView from "@/components/instances-page/instanceView.vue";
 import instanceUpdateView from "@/components/instances-page/instanceUpdateView.vue";
 import redirectUpdateView from "@/components/instances-page/redirectUpdateView.vue";
+import overview from "@/components/instances-page/overview.vue";
 
 import { logout } from "@/bin/auth";
 
@@ -88,7 +79,8 @@ export default {
         countItem,
         instanceView,
         redirectUpdateView,
-        instanceUpdateView
+        instanceUpdateView,
+        overview
     },
 
     data() {
@@ -96,7 +88,7 @@ export default {
             {
                 icon: 'pi pi-refresh',
                 label: "Reload",
-                command: () => console.log("1")
+                command: () => location.reload()
             },
             {
                 icon: 'pi pi-sign-out',
@@ -115,7 +107,8 @@ export default {
                     total: "0.00",
                     used: "0.00"
                 }
-            }
+            },
+            view: 0
         }
     },
     methods: {
@@ -124,11 +117,17 @@ export default {
         },
         fetchSysInfo() {
             this.$STORAGE.socket.emit("get:sysInfo", (data) => this.sys = data);
+        },
+        changeView(i) {
+            this.view = i;
+        },
+        createRedirect() {
+            this.view = 2;
         }
     },
     created() {
         this.fetchSysInfo();
-        this.int = setInterval(() => this.fetchSysInfo(), this.$STORAGE.updateInterval)
+        this.int = setInterval(() => this.fetchSysInfo(), this.$STORAGE.updateInterval);
     },
     unmounted() {
         clearInterval(this.int);
