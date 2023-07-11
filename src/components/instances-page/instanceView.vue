4702fb9fa0cc8349ecd4ed811a5c0145c726fec5
Jonas Schneider <schneider-jonas@mein.gmx>
2023-07-11 19:13:45 +0200

    save

--- src/components/instances-page/instanceView.vue
+++ src/components/instances-page/instanceView.vue
@@ -1,20 +1,12 @@
 <template>
     <div v-if="selectedInstance">
-        <p class="m-0 text-200 mb-3 text-overflow">{{ $t('main-page.instances-comp.instance') }} ({{ selectedInstance.name }})</p>
+        <p class="m-0 text-200 mb-3 text-overflow">{{ $t('main-page.instances-comp.instance') }} ({{ selectedInstance.name
+        }})</p>
         <div class="w-full flex flex-column gap-3 surface-card border-round p-3">
             <div class="flex justify-content-between">
-                <div class="flex align-items-center gap-3 w-8">
-                    <p class="text-3xl m-0 font-bold text-overflow">{{ selectedInstance.name }}</p>
-                    <Chip :label="$t('main-page.instances-comp.running')" icon="pi pi-check" class="text-xs bg-green-600"
-                        v-if="selectedInstance.status == 1"></Chip>
-                    <Chip :label="$t('main-page.instances-comp.stopped')" icon="pi pi-exclamation-triangle" class="text-xs bg-red-600"
-                        v-if="selectedInstance.status == -1"></Chip>
-                    <Chip :label="$t('main-page.instances-comp.stopped')" icon="pi pi-times" class="text-xs bg-red-600" v-if="selectedInstance.status == 0">
-                    </Chip>
-                    <Chip :label="$t('main-page.instances-comp.restarting')" icon="pi pi-stopwatch" class="text-xs bg-orange-600"
-                        v-if="selectedInstance.status == 2"></Chip>
-                    <Chip :label="$t('main-page.instances-comp.updating')" icon="pi pi-stopwatch" class="text-xs bg-blue-600"
-                        v-if="selectedInstance.status == 3"></Chip>
+                <div class="status_indicator" :style="{ '--ind-color': statusColor }">
+                    <div class="dot"></div>
+                    <p>{{ statusLabel }}</p>
                 </div>
                 <div class="flex gap-3">
                     <i class="pi pi-cog text-xl text-gray-400 cursor-pointer"
@@ -23,8 +15,14 @@
                         @click=" this.$EVENT.emit('changeView', 0);"></i>
                 </div>
             </div>
+            <div class="flex justify-content-between align-items-center">
+                <p class="text-3xl m-0 font-bold text-overflow w-9">{{ selectedInstance.name }}</p>
+                    <p class="text-sm m-0 text-gray-400 font-mono">{{ $t('main-page.instances-comp.version') }} {{
+                    selectedInstance.version }}</p>
+            </div>
             <div class="flex align-item-center">
-                <div class="flex gap-2 align-item-center" v-if="selectedInstance.network.isAccessable">
+                <Chip label="Production" class="text-xs bg-grey-600"></Chip>
+                <!-- <div class="flex gap-2 align-item-center" v-if="selectedInstance.network.isAccessable">
                     <i class="pi pi-link text-300"></i>
                     <a class="no-underline text-gray-400 font-mono"
                         :href="`https://${selectedInstance.network.redirect.sub}.${selectedInstance.network.redirect.domain}`"
@@ -33,22 +31,24 @@
                     <a class="no-underline text-gray-400 font-mono"
                         :href="`https://${selectedInstance.network.redirect.domain}`" v-else>{{
                             selectedInstance.network.redirect.domain }}</a>
-                </div>
-                <p class="text-sm ml-auto m-0 text-gray-400 font-mono">{{$t('main-page.instances-comp.version')}} {{ selectedInstance.version }}</p>
+                </div> -->
             </div>
             <span class="p-buttonset w-full flex">
-                <Button :loading="selectedInstance.status == 2" :label="selectedInstance.status == 0 ? $t('main-page.instances-comp.start') : $t('main-page.instances-comp.restart')"
+                <Button :loading="selectedInstance.status == 2"
+                    :label="selectedInstance.status == 0 ? $t('main-page.instances-comp.start') : $t('main-page.instances-comp.restart')"
                     icon="pi pi-refresh" class="p-button-sm flex-auto bg-white-a15 text-white"
                     @click=" startAction(this.selectedInstance.status == 1 ? 2 : 1);"></Button>
-                <Button :label="$t('main-page.instances-comp.stop')" icon="pi pi-stop" class="p-button-sm flex-auto bg-white-a15 text-white"
-                    @click=" startAction(0);"></Button>
-                <Button :label="$t('main-page.instances-comp.update')" icon="pi pi-download" class="p-button-sm flex-auto bg-white-a15 text-white"
-                    @click=" startAction(3);"></Button>
+                <Button :label="$t('main-page.instances-comp.stop')" icon="pi pi-stop"
+                    class="p-button-sm flex-auto bg-white-a15 text-white" @click=" startAction(0);"></Button>
+                <Button :label="$t('main-page.instances-comp.update')" icon="pi pi-download"
+                    class="p-button-sm flex-auto bg-white-a15 text-white" @click=" startAction(3);"></Button>
             </span>
             <p class="m-0 mt-2 text-sm font-mono">{{ $t('main-page.instances-comp.stats') }}</p>
             <div class="flex gap-2">
-                <countItem :str="$t('main-page.ram')" :num="selectedInstance.pm2?.monit.memory || 0" color="var(--white)"></countItem>
-                <countItem :str="$t('main-page.cpu')" :num="selectedInstance.pm2?.monit.cpu || 0" color="var(--white)"></countItem>
+                <countItem :str="$t('main-page.ram')" :num="selectedInstance.pm2?.monit.memory || 0" color="var(--white)">
+                </countItem>
+                <countItem :str="$t('main-page.cpu')" :num="selectedInstance.pm2?.monit.cpu || 0" color="var(--white)">
+                </countItem>
             </div>
             <p class="m-0 mt-2 text-sm font-mono">{{ $t('main-page.instances-comp.logs') }}</p>
             <Textarea class="w-full h-10rem bg-white-a05 border-none overflow-y-scroll font-mono" autoResize="false"
@@ -58,6 +58,8 @@
 </template>
 
 <script>
+/* eslint-disable */
+
 import Button from "primevue/button";
 import Textarea from "primevue/textarea";
 import Chip from 'primevue/chip';
@@ -79,6 +81,26 @@ export default {
     props: {
         selectedInstanceId: String,
         openInstanceUpdateView: Function
+    },
+    computed: {
+        statusColor() {
+            const documentStyle = getComputedStyle(document.documentElement);
+            let i = this.selectedInstance.status;
+            let color = i == 1 ? "--green-600" :
+                i == -1 ? "--red-600" :
+                    i == 0 ? "--red-600" :
+                        i == 2 ? "--orange-600" :
+                            i == 3 ? "--blue-600" : "--red-600";
+            return documentStyle.getPropertyValue(color);
+        },
+        statusLabel() {
+            let i = this.selectedInstance.status;
+            return this.$t(i == 1 ? 'main-page.instances-comp.running' :
+                i == -1 ? 'main-page.instances-comp.error' :
+                    i == 0 ? 'main-page.instances-comp.stopped' :
+                        i == 2 ? 'main-page.instances-comp.restarting' :
+                            i == 3 ? 'main-page.instances-comp.updating' : 'main-page.instances-comp.error')
+        }
     },
     methods: {
         startAction(i) {
@@ -115,5 +137,52 @@ export default {
 
 .bg-white-a05 {
     background-color: rgba(255, 255, 255, 0.05) !important;
+}
+
+.status_indicator {
+    --ind-size: 10px;
+    --ind-color: rgb(253, 65, 55);
+
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    .dot {
+        padding: var(--ind-size);
+        border-radius: 50%;
+        background-color: var(--ind-color);
+
+        position: relative;
+
+        &::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            padding: var(--ind-size);
+            border-radius: 50%;
+            background-color: var(--ind-color);
+            opacity: 0.5;
+            animation: pulse 2s infinite;
+        }
+    }
+
+    p {
+        font-size: 0.8rem;
+        margin: 0 0 2px 0;
+        color: var(--ind-color);
+        text-transform: uppercase;
+    }
+}
+
+@keyframes pulse {
+    0% {
+        transform: scale(1);
+    }
+
+    100% {
+        transform: scale(2.5);
+        opacity: 0;
+    }
 }
 </style>
